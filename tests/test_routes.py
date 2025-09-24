cffab550cd7272818e604eeaa1b35ea41d2ce2a6
AliHassan313 <ali.mughal63@yahoo.com>
2025-09-24 17:35:16 -0400

    tests changed for lint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -132,9 +132,9 @@ class TestAccountService(TestCase):
         response = self.client.get(
             f"{BASE_URL}/{account.id}", content_type="application/json"
         )
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
-        self.assertEqual(data["name"],account.name)
+        self.assertEqual(data["name"], account.name)
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
@@ -158,7 +158,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
-    
 
     def test_delete_account(self):
         """It should Delete an Account"""
